fc510a0160968f77d9fda0d04dfc2ac7e1d429ed
서혜연 <seohyeyeon@seohyeyeon-ui-MacBookAir.local>
2024-06-07 15:22:46 +0900

    feat: 결과 페이지 생성

--- src/app/result/page.js
+++ src/app/result/page.js
@@ -1,11 +1,10 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "../page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>오늘의 럭키푸드</h1>
-      <a href="./result">결과 보러 가기</a>
+      <h1>결과 화면</h1>
     </main>
   );
 }
