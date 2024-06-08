e3c750229b49afcb4da6f4b33d9860506e54be75
Jeongkwonwoo <swan2203@naver.com>
2024-06-09 00:46:55 +0900

    test

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>오늘의 럭키푸드</h1>
+      <h2>테스트</h2>
       <a href="./result">결과 보러 가기</a>
     </main>
   );
